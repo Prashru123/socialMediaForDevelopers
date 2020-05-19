e230e5f45f6690b836bd24e35f1d1e1dd4ff00c9
ICF2025802 <shruthi.g.m@icf.com>
2020-05-19 22:19:05 +0530

    Connection of MongoDB Mongoose

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const app = express();
+const connectDB = require('./config/db');
 
+const app = express();
+//connect to DB
+connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
